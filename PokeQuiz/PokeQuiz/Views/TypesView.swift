ed42491b88ff507b0fdb26ee873aec7a708c8eaf
TannKhtums <69991157+TannKhtums@users.noreply.github.com>
2022-02-16 05:57:21 -0700

    Added TypesView

---
+++ PokeQuiz/PokeQuiz/Views/TypesView.swift
@@ -0,0 +1,20 @@
+//
+//  TypesView.swift
+//  PokeQuiz
+//
+//  Created by Tanner Garlick on 2/15/22.
+//
+
+import SwiftUI
+
+struct TypesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TypesView_Previews: PreviewProvider {
+    static var previews: some View {
+        TypesView()
+    }
+}
